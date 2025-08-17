d0da3cd77ab04237113afc9296fc7f400ac88785
Yaroslav Mihalev <j.mihalev1018@yandex.ru>
2025-08-17 18:38:40 +0300

    feat: VK ID

--- api/src/services/auth/auth.controller.ts
+++ api/src/services/auth/auth.controller.ts
@@ -71,6 +71,7 @@ export class AuthController {
     res.clearCookie('access_token');
   }
 
+  @Public()
   @Post('/vk')
   async authVK(@Res({ passthrough: true }) res) {
     console.log(res.body);
