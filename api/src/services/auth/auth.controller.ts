d3ee1d1c10efe3d0fcd94e4364e076580ebb9ed5
Yaroslav Mihalev <j.mihalev1018@yandex.ru>
2025-08-18 20:57:27 +0300

    feat: VK ID

--- api/src/services/auth/auth.controller.ts
+++ api/src/services/auth/auth.controller.ts
@@ -94,13 +94,13 @@ export class AuthController {
     console.log('response1 status', JSON.stringify(response.status));
     console.log('response1 data', JSON.stringify(response.data));
 
-    const access_token = response.data.access_token;
-    console.log('access_token', access_token);
+    const id_token = response.data.id_token;
+    console.log('id_token', id_token);
 
     const response2 = await firstValueFrom(
       this.httpService.post('https://id.vk.com/oauth2/public_info', {
         client_id: process.env.CLIENT_ID,
-        id_token: access_token,
+        id_token,
       }),
     );
 
