53587fa8054770c5bed62415b002d058eda5de11
Yaroslav Mihalev <j.mihalev1018@yandex.ru>
2025-08-22 20:40:05 +0300

    feat: енвы

--- api/src/db/db.config.ts
+++ api/src/db/db.config.ts
@@ -3,11 +3,12 @@ import * as dotenv from 'dotenv';
 import * as path from 'path';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
-dotenv.config({ path: process.cwd() + `/.env.${process.env.NODE_ENV}` });
+dotenv.config({ path: process.cwd() + `/.env` });
 
 export default registerAs('dbconfig.dev', (): PostgresConnectionOptions => {
   console.log('registerAs db config');
   console.log(process.env);
+  console.log(process.env.POSTGRES_USERNAME);
   return {
     type: 'postgres',
     host: process.env.POSTGRES_HOST,
